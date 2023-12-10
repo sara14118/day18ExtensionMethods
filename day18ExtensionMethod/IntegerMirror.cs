49e4abff2abe48d3422e6503008d7ff8e2ea5d82
sara <sara.alzadjali14@outlook.com>
2023-12-10 13:51:41 +0400

    Add project files.

---
+++ day18ExtensionMethod/IntegerMirror.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day18ExtensionMethod
+{
+    internal static class IntegerMirror
+    {
+        public static int Mirror (this int number)
+        {
+            int result = 0, remainder;
+
+            while (number != 0) {
+
+                remainder = number % 10;
+                result = result * 10 + remainder;
+                number = number / 10;
+            }
+            return result;
+
+           
+
+
+
+        }
+    }
+}
---
+++ day18ExtensionMethod/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace day18ExtensionMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 12345;
+            //Console.WriteLine(x.Mirror());
+
+
+            //var emp = new {id = 1, name = "sara", salary = 1000};
+
+            //Console.WriteLine(emp.GetType().Name);
+
+
+            //List <int > list1 = new List<int> { 1,2,3,4,5,6,7,8,9};
+            //List <int> result = Enumerable.Where (list1, x => x%2 != 0).ToList ();
+            //foreach (var x in result)
+            //{
+            //    Console.WriteLine (x);
+
+
+            //}
+
+
+
+
+
+
+            List<int> lst = new List<int>() { 1,2,3,4,5,6,7,8,9};
+            var ls = lst.Where(i=> i%2==0).ToList();
+            Console.WriteLine(lst.GetType());
+
+            lst.Remove(2);
+            lst.AddRange(new int[] {10,11,12,13,14});
+
+            foreach (int i in ls.ToList())
+            {
+                Console.WriteLine($"{i}");
+            }
+            lst.AddRange(new int[] { 15,16, 17, 18, 19 });
+
+            Console.WriteLine("  ");
+
+            foreach (int i in ls)
+            {
+                Console.WriteLine($"{i}");
+            }
+            Console.WriteLine("");
+
+
+
+
+        }
+    }
+}
